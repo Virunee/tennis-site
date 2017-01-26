a2226f3a44342404618ae8de4b04c9cf071a3871
Virunee <viravongv@gmail.com>
2017-01-26 21:38:12 +0000

    Added routing

---
+++ app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module('myApp', ['ngMessages', 'ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+  $routeProvider
+
+    .when('/!', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+
+    .when('/aboutus', {
+        templateUrl: 'pages/aboutus.html',
+        controller: 'mainController'
+    })
+});
+
+
+myApp.controller('mainController', ['$scope', '$filter', '$location', function($scope, $filter, $location) {
+
+}]);
